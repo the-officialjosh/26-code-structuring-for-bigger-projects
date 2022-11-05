6d907729338ccebdba535bf8658fab6579604a16
JOSH <onyemajoshua892@gmail.com>
2022-11-05 02:09:33 +0100

    save

---
+++ src/Experience/World/World.js
@@ -0,0 +1,19 @@
+import * as THREE from 'three';
+import Experience from '../Experience';
+import Environment from './Environment';
+import Floor from './Floor';
+
+export default class World {
+    constructor() {
+        this.experience = new Experience();
+        this.scene = this.experience.scene;
+        this.resources = this.experience.resources;
+
+        //Wait for resources
+        this.resources.on('ready', () => {
+            //Setup
+            this.floor = new Floor();
+            this.environment = new Environment();
+        });
+    }
+}
